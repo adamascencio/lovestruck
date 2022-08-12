12fbc469cddeb78392cb29ec174333476d9e5643
adamascencio <ascencioadam@gmail.com>
2022-08-12 16:49:01 -0700

    Add location model to models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
+
+
+class Location(models.Model):
+  name = models.CharField(max_length=100)
+  address = models.CharField(max_length=200)
+  phone_num = models.CharField(max_length=14)
+  city = models.CharField(max_length=50)
+  state = models.CharField(max_length=2)
+  category = models.CharField(max_length=50)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self): 
+    return f'Name: {self.name}, ({self.id})'
