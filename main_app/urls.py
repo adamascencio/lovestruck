53b851508498743391a17451c446f060b6e98a70
adamascencio <ascencioadam@gmail.com>
2022-08-14 14:50:02 -0700

    add location create functionality and user signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('location/', views.LocationList.as_view(), name='location_index'),
+    path('location/create/', views.LocationCreate.as_view(), name='location_create'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
