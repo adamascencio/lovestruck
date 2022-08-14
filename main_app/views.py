5b5d62f8ecb50b657f2965528b496130f07e780b
adamascencio <ascencioadam@gmail.com>
2022-08-14 13:33:59 -0700

    Add location index functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Location
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+class LocationList(ListView):
+    model = Location
+    fields = ['name', 'category', 'city', 'state']
